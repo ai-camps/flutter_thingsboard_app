138cdf461b3226eed5232f72e19ea52c8832308c
Jun Wen <jun.wen@ai-camps.com>
2024-11-12 11:27:22 -0800

    updates

--- lib/constants/app_constants.dart
+++ lib/constants/app_constants.dart
@@ -1,5 +1,5 @@
 abstract class ThingsboardAppConstants {
-  static const thingsBoardApiEndpoint = 'http://73.223.62.174:8080';
+  static const thingsBoardApiEndpoint = 'http://192.168.1.200:8080';
   static const thingsboardOAuth2CallbackUrlScheme = 'org.thingsboard.app.auth';
 
   /// Not for production (only for debugging)
